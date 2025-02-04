7ea60c5d789ce5fdc5811a19a584024de0424719
htai <huutaiyq115@gmail.com>
2025-02-04 13:44:08 +0700

    new

--- src/main/java/com/storemanager/store_management/entity/Customer.java
+++ src/main/java/com/storemanager/store_management/entity/Customer.java
@@ -1,17 +1,16 @@
 package com.storemanager.store_management.entity;
 
-
-
-
 import jakarta.persistence.*;
-import lombok.Getter;
+import lombok.*;
 
 import java.util.Date;
 
 @Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "customers")
-
 public class Customer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -36,46 +35,4 @@ public class Customer {
     @Column(name = "updated_at")
     @Temporal(TemporalType.TIMESTAMP)
     private Date updatedAt = new Date();
-
-    public Customer() {
-    }
-
-    public Customer(Long id, String name, String phone, String address, Double debtBalance, Date createdAt, Date updatedAt) {
-        this.id = id;
-        this.name = name;
-        this.phone = phone;
-        this.address = address;
-        this.debtBalance = debtBalance;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public void setDebtBalance(Double debtBalance) {
-        this.debtBalance = debtBalance;
-    }
-
-    public void setCreatedAt(Date createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public void setUpdatedAt(Date updatedAt) {
-        this.updatedAt = updatedAt;
-    }
 }
-
